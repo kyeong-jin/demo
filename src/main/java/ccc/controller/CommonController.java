efc5755e793a301a9b4cc2eb53909c2658296388
kyeong-jin <82008939+kyeong-jin@users.noreply.github.com>
2021-04-19 17:15:36 +0900

    Update CommonController.java

--- src/main/java/ccc/controller/CommonController.java
+++ src/main/java/ccc/controller/CommonController.java
@@ -6,12 +6,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class CommonController {
 
 	@GetMapping("/main")
 	public String dashboard1(Model model, HttpSession session) {
         model.addAttribute("sessionId", session.getId());
+        log.debug("sessionId : {}", session.getId());
 		return "main";
 	}
 }
